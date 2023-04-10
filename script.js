4e180aaac0c5b68ce5482ab5ca9172d964b37938
Valevmu <madridvalentina00@gmail.com>
2023-04-10 16:07:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeText(){
+  document.getElementById('changeName').innerHTML = 'Valentina Madrid';
+}
+
+function removeUser() {
+  const element = document.getElementById("quit");
+  element.remove();
+}
